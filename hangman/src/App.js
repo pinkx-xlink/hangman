f7fb674ddb35ac0aa3b2ef0cec488b4bd53f94d5
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-12 18:48:01 -0500

    Import useEffect, and call setWrongLetters or  setCorrectLetters when user guesses a letter

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
@@ -18,6 +18,37 @@ function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      window.addEventListener('keydown', e => {
+        if () {
+          if (playable && keycode >= 65 && e.keycode <= 90) {
+            const letter = e.key.toLowerCase();
+    
+            if(selectedWord.includes(letter)) {
+              if (!correctLetters.includes(letter)) {
+                setCorrectLetters(currentLetters => [...currentLetters, letter]);
+    
+                // displayWord();
+              } else {
+                // showNotification();
+              } 
+            } else {
+                if (!wrongLetters.includes(letter)) {
+                  setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+                } else {
+                  // showNotification();
+                }
+              }
+            }
+          }
+          
+  });
+
+  
+  })
   return (
     <>
       <Header />
