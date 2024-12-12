d899a6ecf944e2a87901875b58136f3401a25b1a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-12 18:00:42 -0500

    Create and export Notification component

---
+++ hangman/src/components/Notification.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Notification = () => {
+    return (
+        <div class="notifications-container" id="notifications-container">
+            <p>You have already entered this letter</p>
+        </div>
+    )
+}
+
+export default Notification
