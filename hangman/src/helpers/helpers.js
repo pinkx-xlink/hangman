73a65ef2ea9e0c578cfe6298e341ab39451fa42b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-12 19:41:22 -0500

    Create checkWin(), which checks whether we win or lost

--- hangman/src/helpers/helpers.js
+++ hangman/src/helpers/helpers.js
@@ -5,4 +5,20 @@ function showNotification(setter) {
   }, 2000);
 }
 
-export default showNotification;
+export function checkWin(correct, wrong, word) {
+  // we won
+  let status = 'win';
+
+  // check for win - we haven't won or lost
+  word.split('').forEach(letter => {
+    // if correct includes a letter for our word, we don't win or lose
+    if(!correct.includes(letter)){
+      status = '';
+    }
+  })
+
+  // check for loss - we lost. 
+  if(wrong.length === 6) status = 'lose';
+
+  return status;
+}
